b9444d127109ce6e511a85a150330797d5e0c904
cchinniah <30440607+cchinniah@users.noreply.github.com>
2023-04-22 11:21:55 -0400

    Update streamlit_app.py
    Added code so users can pick the fruit from the list.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,9 @@ streamlit.text(' 🐔 Hard -Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build your Own Fruit Smoothie 🥝🍇' )
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Lets puta a pick list here so tey can pick the fruis they want to include
+streamlit.multiselect("Pick some fruits", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
 
