41e238b57defaf0f5b3c8cb951986a77d1459e56
cchinniah <30440607+cchinniah@users.noreply.github.com>
2023-04-23 22:50:45 -0400

    Update streamlit_app.py
    Provided an example fruit list  'Avocado','Strawberries' so the customer knows what to pick.

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,11 @@ streamlit.header('🍌🥭 Build your Own Fruit Smoothie 🥝🍇' )
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit');
 # Lets puta a pick list here so tey can pick the fruis they want to include
-streamlit.multiselect("Pick some fruits", list(my_fruit_list.index)) 
+#streamlit.multiselect("Pick some fruits", list(my_fruit_list.index)) -- 1st
+#We want to filter the table data based on the fruits a customer will choose, so we'll pre-populate the list to set an example for the customer. 
+streamlit.multiselect("Pick some fruits", list(my_fruit_list.index),['Avocado','Strawberries']) 
 streamlit.dataframe(my_fruit_list)
+
+
 
 
