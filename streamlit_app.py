f126670ec5c846b359c0f27d362417fe071571fe
cchinniah <30440607+cchinniah@users.noreply.github.com>
2023-04-25 17:19:07 -0400

    Update streamlit_app.py
    Added the requests module to be imported. made a call to the request.get to get the data for watermelon using the fruityvice api created.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,12 @@ Finally, we'll ask the app to use the data in fruits_to_show in the dataframe it
 fruits_selected = streamlit.multiselect("Pick some fruits", list(my_fruit_list.index),['Avocado','Strawberries']) 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+#New section to show fruitvice api response
+streamlit.header('Fruityvice Fruit Advice')
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
 
 
 
