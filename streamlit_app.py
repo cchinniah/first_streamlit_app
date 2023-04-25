ebf5bc742fcd0bc397b67dbf594128336f5b8de9
cchinniah <30440607+cchinniah@users.noreply.github.com>
2023-04-25 18:05:19 -0400

    Update streamlit_app.py
    debugging as the dataframe was not displaying the values. Removed the comment on the normalization of json using pandas.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,14 +26,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 #New section to show fruitvice api response
 streamlit.header('Fruityvice Fruit Advice')
-#fruit_choice = streamlit.text_input('What fruit would you like information about?' ,'Kiwi')
-#streamlit.write('The user entered',fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?' ,'Kiwi')
+streamlit.write('The user entered',fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/Kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 streamlit.text(fruityvice_response.json())# Just writes the data to screen as a test.
 #Take the json version of the response and normalize it
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it in the screen as table
 streamlit.dataframe(fruityvice_response)
 
