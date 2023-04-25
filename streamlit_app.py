5c4984f17c1ed17c7460bf01d01f39760b4e2c87
cchinniah <30440607+cchinniah@users.noreply.github.com>
2023-04-25 17:23:31 -0400

    Update streamlit_app.py
    Format the raw json data using the pandas json_normalize function.

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,11 @@ streamlit.header('Fruityvice Fruit Advice')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json())# Just writes the data to screen
+#Take the json version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice.json())
+#output it in the screen as table
+streamlit.dataframe(fruityvice_normalized)
 
 
 
