086b430820b801bd0063d67e75818221daa2d7ef
cchinniah <30440607+cchinniah@users.noreply.github.com>
2023-04-26 17:07:20 -0400

    Update streamlit_app.py
    We were able to connect and get the connection details. Now modified select to fetch the data from the table.

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,10 +40,14 @@ streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select current_user(),current_account(),current_region()")
+#streamlit.text("Hello from snowflake:")
+#my_cur.execute("select current_user(),current_account(),current_region()")
+my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from snowflake:")
+streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
+
+
 
 
 
